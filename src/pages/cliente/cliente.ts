931aa67c7b3b120a6817991fb40f26bdf87e6578
Pedro Dantas <pedro_pdantas@hotmail.com>
2019-06-07 14:57:39 -0300

    Criando formGroup para controlar o formulário

--- src/pages/cliente/cliente.ts
+++ src/pages/cliente/cliente.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 
 @IonicPage()
@@ -9,14 +10,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ClientePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
+    this.formGroup = this.formBuilder.group({
+      nome: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+      email: ['', [Validators.required, Validators.email]],
+      cpf: ['', [Validators.required, Validators.minLength(11)]],
+      numero: ['', []],
+      complemento: ['', []],
+      bairro: ['', []],
+      cep: ['', [Validators.required]],
+      telefone1: ['', [Validators.required]],
+      telefone2: ['', [Validators.required]],
+      telefone3: ['', [Validators.required]],
+      cidadeId: [null, [Validators.required]],
+      estadoId: [null, [Validators.required]]
+
+    });
   }
+  formGroup: FormGroup
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ClientePage');
   }
 
   signupUser(){
-    
+
   }
 }
